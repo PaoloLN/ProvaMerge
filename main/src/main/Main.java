e9b5b4a0b1b53fba86354353d8d6f3816d5c8eda
PaoloLN <101583538+PaoloLN@users.noreply.github.com>
2022-03-31 18:50:58 +0200

    Add files via upload

---
+++ main/src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("A");
+        
+        System.out.println("B");
+        System.out.println("C");
+        
+        
+        
+        
+        
+    }
+
+}
